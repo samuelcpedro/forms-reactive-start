6aa853bc9e120c8bde142e32c5ed900c618b95b2
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-16 13:10:17 -0100

    Reactive: Creating Custom Validators
    Reactive: Creating Custom Validators
    Section 15, Lecture 200

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     // Add 'implements OnInit' to the class.
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required), /* required and email are functions so dont put () or it will be executed */
+        /* we need to bind this because the validation function is called by angular no by this class */
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
+        /* required and email are functions so dont put () or it will be executed */
       }),
       'gender': new FormControl('male'),
       // 'hobbies': new FormArray([new FormControl(null)])
@@ -31,5 +34,19 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  /* Validator is a function that checks the validity of a formControl*/
+  /* This function should return something like this:
+   { nameIsForbbiden: true }
+  */
+  forbiddenNames(control: FormControl): {[s: string]: boolean } {
+    /* This validation is returning -1 and -1 is true */
+    /* if (this.forbiddenUsernames.indexOf(control.value)) { */
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
+    }
+    return null;
+    /* If successfull we should return nothing or null never pass { 'nameIsForbidden': false } */
   }
 }
